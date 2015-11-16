1fc8ec485c460d29336a75250b0ef42a65b4b779
zhu.gongping <zhu.gongping@kjt.com>
2015-11-16 17:39:29 +0800

    新增sayHello 事务实现demo

--- s2s-service-impl/src/main/java/com/oimboi/service/s2s/HelloImpl.java
+++ s2s-service-impl/src/main/java/com/oimboi/service/s2s/HelloImpl.java
@@ -1,11 +1,28 @@
 package com.oimboi.service.s2s;
 
+import javax.annotation.Resource;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oimboi.service.s2s.dao.ICodeDAO;
+import com.oimboi.service.s2s.dao.model.Code;
+
 public class HelloImpl implements IHello {
-
+	@Resource(name="Code")
+	private ICodeDAO<Code> codeDAOImpl;
+	
 	@Override
+	@Transactional
 	public void sayHello() {
-		// TODO Auto-generated method stub
-
+		Code model = new Code();
+		model.setName("siling");
+		codeDAOImpl.insert(model, null);
+		model = new Code();
+		model.setName("siling");
+		codeDAOImpl.insert(model, null);
+		model = new Code();
+		model.setName("siling");
+		codeDAOImpl.insert(model, null);
 	}
 
 }
--- s2s-service-impl/src/test/java/com/oimboi/service/s2s/HelloImplTest.java
+++ s2s-service-impl/src/test/java/com/oimboi/service/s2s/HelloImplTest.java
@@ -1,5 +1,7 @@
 package com.oimboi.service.s2s; 
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -9,11 +11,12 @@ import com.tower.service.annotation.SoaFwTest;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration( locations = { "classpath*:/META-INF/config/spring/spring-service.xml"})
 public class HelloImplTest {
+	@Resource(name="helloService")
+	private IHello hello;
 	@Test
 	@SoaFwTest(id="1a207b9f23579e35d1dfdd4e555ba9fa", method="sayHello", params={"sayHello"})
 	public void sayHello() {
-
-		throw new RuntimeException("sayHello test not implemented");
+		hello.sayHello();
 	}
 
 }
