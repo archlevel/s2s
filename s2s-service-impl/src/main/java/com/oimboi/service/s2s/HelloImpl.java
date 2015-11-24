095dabb5312aa9e8b5951b916e7480ded18c5d71
zhu.gongping <zhu.gongping@kjt.com>
2015-11-24 15:02:49 +0800

    新增plugin version

--- s2s-service-impl/src/main/java/com/oimboi/service/s2s/HelloImpl.java
+++ s2s-service-impl/src/main/java/com/oimboi/service/s2s/HelloImpl.java
@@ -6,14 +6,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.oimboi.service.s2s.dao.ICodeDAO;
 import com.oimboi.service.s2s.dao.model.Code;
-import com.tower.service.aop.LoggerPoint;
+import com.tower.service.aop.Loggable;
 
 public class HelloImpl implements IHello {
 	@Resource(name="Code")
 	private ICodeDAO<Code> codeDAOImpl;
 	
 	@Override
-	@LoggerPoint
+	@Loggable
 	@Transactional
 	public void sayHello() {
 		Code model = new Code();
